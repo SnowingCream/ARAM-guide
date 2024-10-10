a1237a4e80ce69e16238da70c6901c967afe3199
Snowing Cream <68310688+Scombo2020@users.noreply.github.com>
2024-10-10 18:03:54 -0500

    added icon for sorting direction indication

--- client/src/component/ChampionSummaryContainer.jsx
+++ client/src/component/ChampionSummaryContainer.jsx
@@ -109,7 +109,37 @@ function ChampionSummaryContainer(props) {
           }}
         />
         <label className="btn btn-outline-secondary" htmlFor="btnradio1">
-          # Games
+          # Games{" "}
+          {sortCriterion === "numberOfPlays" &&
+            (isAscending ? (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                className="bi bi-chevron-down"
+                viewBox="0 0 16 16"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708"
+                />
+              </svg>
+            ) : (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                className="bi bi-chevron-up"
+                viewBox="0 0 16 16"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M7.646 4.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1-.708.708L8 5.707l-5.646 5.647a.5.5 0 0 1-.708-.708z"
+                />
+              </svg>
+            ))}
         </label>
 
         <input
@@ -125,7 +155,37 @@ function ChampionSummaryContainer(props) {
           }}
         />
         <label className="btn btn-outline-secondary" htmlFor="btnradio2">
-          Winning Rate
+          Winning Rate{" "}
+          {sortCriterion === "winningRate" &&
+            (isAscending ? (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                className="bi bi-chevron-down"
+                viewBox="0 0 16 16"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708"
+                />
+              </svg>
+            ) : (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                className="bi bi-chevron-up"
+                viewBox="0 0 16 16"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M7.646 4.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1-.708.708L8 5.707l-5.646 5.647a.5.5 0 0 1-.708-.708z"
+                />
+              </svg>
+            ))}
         </label>
 
         <input
@@ -141,7 +201,37 @@ function ChampionSummaryContainer(props) {
           }}
         />
         <label className="btn btn-outline-secondary" htmlFor="btnradio3">
-          KDA
+          KDA{" "}
+          {sortCriterion === "KDA" &&
+            (isAscending ? (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                className="bi bi-chevron-down"
+                viewBox="0 0 16 16"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708"
+                />
+              </svg>
+            ) : (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                className="bi bi-chevron-up"
+                viewBox="0 0 16 16"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M7.646 4.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1-.708.708L8 5.707l-5.646 5.647a.5.5 0 0 1-.708-.708z"
+                />
+              </svg>
+            ))}
         </label>
       </div>
 
