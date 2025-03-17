ae76367f53de06c9f3a0d0aa63dda764ab78686e
Snowing Cream <68310688+Scombo2020@users.noreply.github.com>
2025-03-16 23:51:01 -0500

    synchronized the port number and added CORS for the communication with frontend in deployment

--- server/server.js
+++ server/server.js
@@ -1,13 +1,24 @@
 import express from "express";
 import test from "./api.js";
+import cors from "cors";
 
 const app = express();
 const port = 4001;
 
 app.use(express.json());
 
+// Allow frontend server's IP
+// CORS middleware must be applied before routes
+app.use(
+  cors({
+    origin: "http://3.138.68.148", // Frontend public IP
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
+
 app.use("/api", test);
 
 app.listen(port, () => {
-    console.log(`Listening on port ${port}`);
+  console.log(`Listening on port ${port}`);
 });
