543c33cb31019df0d29b90054924e7dda8a20ddd
Snowing Cream <68310688+Scombo2020@users.noreply.github.com>
2025-03-16 23:49:54 -0500

    handled CORS for deployment

--- server/api.js
+++ server/api.js
@@ -1,28 +1,21 @@
 import express from "express";
-import pool from "./db.js";
 import APICall from "./src/APICall.js";
 import DBInsert from "./src/DBInsert.js";
-
-import {
-  DB_accounts_query,
-  DB_match_account_query,
-  DB_matches_query,
-} from "./src/DBQueries.js";
 
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.send({ test: "hi from ARAM!!!!!!!!" });
+  res.send("HI from ARAM GUIDE!!!!!!!!");
 });
 
 router.get("/champion", (req, res) => {
-  res.send({ test: "champion detected" });
+  res.send("CHAMPION ENDPOINT DETECTED!!!");
 });
 
 router.post(`/player`, (req, res) => {
   (async () => {
     const DBData = await APICall(req.body, res);
-    await DBInsert(DBData);
+    // await DBInsert(DBData); -> DB setup later
   })();
 });
 
