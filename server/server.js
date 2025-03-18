23938e9568641bc6ff20d0856d5e1a6200b05fe8
Snowing Cream <68310688+Scombo2020@users.noreply.github.com>
2025-03-18 17:05:58 -0500

    added backend log middleware 'morgan'

--- server/server.js
+++ server/server.js
@@ -2,6 +2,9 @@ import express from "express";
 import test from "./api.js";
 import cors from "cors";
 import dotenv from "dotenv";
+import morgan from "morgan";
+import fs from "fs";
+import path from "path";
 
 const app = express();
 const port = 4001;
@@ -10,6 +13,21 @@ const port = 4001;
 dotenv.config();
 
 app.use(express.json());
+
+// === Morgan setup ===
+
+// 1. Create a write stream (in append mode) for saving logs to file
+const accessLogStream = fs.createWriteStream(path.join("/app", "access.log"), {
+  flags: "a",
+});
+
+// 2. Set up morgan middleware: log to console + file
+app.use(
+  morgan(
+    ":remote-addr :method :url :status :res[content-length] - :response-time ms",
+    { stream: accessLogStream }
+  )
+);
 
 // Allow frontend server's IP
 // CORS middleware must be applied before routes
